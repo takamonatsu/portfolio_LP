53d6e1b3a4a63868dd030bf34460bdde1256826e
takamonatsu <takataka521monatsu7191820@gmail.com>
2025-05-06 13:11:04 +0900

    Add glide Fix products layout

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,7 @@ import { handleCollectionPhotos } from './handleCollectionPhotos';
 import { blockDummyLink } from './blockDummyLink';
 import { hideLoadingScreen } from './hideLoadingScreen';
 import { rotateMainVisualPhotos } from './rotateMainVisualPhotos';
+import { initGlideSlider } from './initGlideSlider';
 
 // DOMContentLoadedイベントで初期化
 document.addEventListener('DOMContentLoaded', () => {
@@ -11,6 +12,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
     blockDummyLink();
     handleCollectionPhotos();
+    initGlideSlider();
     
     // スクロール処理の初期化
     // 画面途中でリロードすると、画面上部で Observer が実行された後に本来の表示位置へ移動してしまうため、遅延させて実行
--- src/main.js
+++ src/main.js
@@ -1,2 +1,6 @@
+// Required Core Stylesheet
+import '@glidejs/glide/dist/css/glide.core.min.css'
+// Optional Theme Stylesheet
+import '@glidejs/glide/dist/css/glide.theme.css'
 import './scss/style.scss'
 import './js/index'
